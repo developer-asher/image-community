dda11431b65cd72e43a696b5bb96775d3e85b69f
Asher <grit.developer.asher@gmail.com>
2021-10-02 02:24:00 +0900

    Fix waste of resources code in input component

--- src/elements/input.jsx
+++ src/elements/input.jsx
@@ -1,62 +1,36 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const Input = (props) => {
-  const inputRef = useRef();
-  const {
-    type,
-    label,
-    placeholder,
-    width,
-    margin,
-    padding,
-    outline,
-    ft_size,
-    onChange,
-  } = props;
-  const styles = { width, margin, padding, outline, ft_size };
+  const { type, label, placeholder, onChange } = props;
   const id = Date.now();
 
   return (
     <>
-      <LabelBox {...styles} htmlFor={id}>
-        {label}
-      </LabelBox>
+      <LabelBox htmlFor={id}>{label}</LabelBox>
       <InputBox
-        ref={inputRef}
         type={type}
         id={id}
         placeholder={placeholder}
         onChange={onChange}
-        {...styles}
       ></InputBox>
     </>
   );
 };
 
 Input.defaultProps = {
-  // property
   type: 'text',
   placeholder: '',
-
-  // style
-  width: '100%',
-  margin: false,
-  padding: false,
-  outline: 'none',
-  ft_size: '14px',
 };
 
 const InputBox = styled.input`
-  width: ${(props) => props.width};
-  ${(props) => (props.margin ? `margin: ${props.margin};` : '')};
-  ${(props) => (props.padding ? `padding: ${props.padding};` : '')};
-  font-size: ${(props) => props.ft_size};
-  outline: ${(props) => props.outline};
+  width: 100%;
+  padding: 10px;
+  outline: none;
 `;
 
 const LabelBox = styled.label`
-  width: ${(props) => props.width};
+  width: 100%;
 `;
 
 export default Input;

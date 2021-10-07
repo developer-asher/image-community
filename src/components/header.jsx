c90cbb086371886c2ff71de373533a76dc2fa2ad
Asher <grit.developer.asher@gmail.com>
2021-10-07 20:04:05 +0900

    Add welcome user in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { Grid, Image, Button } from '../elements/index';
+import { Grid, Image, Button, Text } from '../elements/index';
 import { actionCreators as userActions } from '../redux/modules/user';
 import { history } from '../redux/configureStore';
 import NotiBadge from './notiBadge';
@@ -10,6 +10,7 @@ import NotiBadge from './notiBadge';
 const Header = (props) => {
   const dispatch = useDispatch();
   const is_login = useSelector((state) => state.user.is_login);
+  const user_name = useSelector((state) => state.user?.user?.nick_name);
 
   const goSignIn = () => {
     history.push('/signin');
@@ -35,14 +36,16 @@ const Header = (props) => {
 
   if (is_login) {
     return (
-      <Grid
-        is_flex
-        width='calc(100% - 40px)'
-        margin='0 20px'
-        padding='20px'
-        bg='#fff'
-      >
-        <Image />
+      <Grid is_flex margin='0 0 20px 0' padding='20px' bg='#fff'>
+        <Grid is_flex content_pos='flex-start'>
+          <Image />
+          <Text margin='0 0 0 10px' size='14px'>
+            <strong>{user_name} </strong>
+            <span style={{ fontSize: '13px', color: '#555' }}>
+              님 환영합니다
+            </span>
+          </Text>
+        </Grid>
         <ButtonWrap>
           <Button onClick={editUserInfo}>내정보</Button>
           <Grid margin='0 10px' position>
@@ -55,14 +58,9 @@ const Header = (props) => {
   }
 
   return (
-    <Grid
-      is_flex
-      width='calc(100% - 40px)'
-      margin='0 20px'
-      padding='20px'
-      bg='#fff'
-    >
+    <Grid is_flex margin='0 0 20px 0' padding='20px' bg='#fff'>
       <Image />
+      <Text>{user_name}</Text>
       <ButtonWrap>
         <Button onClick={goSignUp} margin='0 10px 0 0'>
           회원가입
@@ -77,17 +75,6 @@ const ButtonWrap = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-`;
-
-const NoticeCount = styled.span`
-  position: absolute;
-  right: -10px;
-  top: -15px;
-  border-radius: 50%;
-  padding: 6px;
-  background-color: salmon;
-  color: #fff;
-  font-size: 13px;
 `;
 
 export default Header;

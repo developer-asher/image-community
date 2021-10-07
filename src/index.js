f699c5ce420231eb87fef9135c2f6422b75d534d
Asher <grit.developer.asher@gmail.com>
2021-10-07 15:28:14 +0900

    Add report web vitals in firebase analytics

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './shared/App';
+import reportWebVitals from './reportWebVitals';
+
 // 스토어 주입
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
+import { analytics } from './shared/firebase';
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,3 +15,13 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
+
+function sendToAnalytics(metric) {
+  const report = JSON.stringify(metric);
+
+  analytics.logEvent('web_vital_report', report);
+
+  console.log({ report });
+}
+
+reportWebVitals(sendToAnalytics);
